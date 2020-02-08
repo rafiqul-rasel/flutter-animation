e89ad3b98f2d844eb240cfd530f5bed74c07e6ae
Md.Rafiqul Islam <mrafiqulislam@duet.ac.bd>
2020-02-09 02:42:50 +0600

    animation builder for animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/animation.dart';
+
 
 void main()=>runApp(MaterialApp(home: MyApps(),));
 class MyApps extends StatefulWidget {
@@ -7,27 +7,47 @@ class MyApps extends StatefulWidget {
   _MyAppsState createState() => _MyAppsState();
 }
 
-class _MyAppsState extends State<MyApps> with SingleTickerProviderStateMixin {
-  Animation<double> animation;
-  AnimationController controller;
+class _MyAppsState extends State<MyApps> with TickerProviderStateMixin {
+Animation animation;
+AnimationController controller;
 
-  @override
+@override
   void initState() {
-    super.initState();
-    controller=AnimationController(duration: Duration(milliseconds: 5000),vsync: this);
-    animation= Tween(begin: 0.0,end: 400.0).animate(controller);
-    animation.addListener((){
-      setState(() {
-
-      });
+    controller=AnimationController(duration:Duration(milliseconds: 1000),vsync: this);
+    CurvedAnimation curvedAnimation=CurvedAnimation(parent: controller, curve: Curves.bounceInOut);
+    animation=Tween(begin: 0.0,end: 360.0).animate(curvedAnimation);
+    animation.addStatusListener((AnimationStatus animationStatus){
+      if(animationStatus==AnimationStatus.completed){
+        controller.reverse();
+      }else if(animationStatus==AnimationStatus.dismissed){
+        controller.forward();
+      }
     });
     controller.forward();
+    super.initState();
   }
+
+  Widget rasel(BuildContext context,Widget child){
+  return Transform.rotate(
+    angle: animation.value,
+    child: Container(
+      height: 200.0,
+      width: 200.0,
+      color: Colors.transparent,
+      child: FlutterLogo(size: animation.value,),
+    ),
+  );
+
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Container(child: FlutterLogo(size: animation.value,),)),
+      body: Center(
+          child:AnimatedBuilder(animation: animation, builder: rasel)
+      )
     );
   }
 }
